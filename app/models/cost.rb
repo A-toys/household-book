718d570b37c04e0164f9752513449874118265f5
A-toys <nao_lespaul_jr@yahoo.co.jp>
2019-11-28 20:50:27 +0900

    fix cost.rb file

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -1,20 +1,4 @@
 class Cost < ApplicationRecord
-  enum :category [:food,
-                  :daily_necessities,
-                  :transportation,
-                  :book,
-                  :miscellaneous,
-                  :clothing_costs,
-                  :entertainment,
-                  :medical_bills,
-                  :rent,
-                  :communication_costs,
-                  :expence,
-                  :electricity_bill,
-                  :water_bill,
-                  :gas_bill,
-                  :insurance,
-                  :tuition
-                ]
+  enum :category [:food, :daily_necessities, :transportation, :book, :miscellaneous, :clothing_costs, :entertainment, :medical_bills, :rent, :communication_costs, :expence, :electricity_bill, :water_bill, :gas_bill, :insurance, :tuition]
 
 end
