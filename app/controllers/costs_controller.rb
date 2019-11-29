435e6653e338b7d89fd737e2a6f54ff02ede642f
A-toys <nao_lespaul_jr@yahoo.co.jp>
2019-11-29 12:52:20 +0900

    add create action

--- app/controllers/costs_controller.rb
+++ app/controllers/costs_controller.rb
@@ -4,4 +4,16 @@ class CostsController < ApplicationController
     @cost = Cost.new
   end
 
+  def create
+    Cost.create(cost_params)
+    redirect_to root_path
+  end
+
+
+  private
+
+  def cost_params
+    params.require(:cost).permit(:category, :price, :content, :date)
+  end
+
 end
