d96fbeb3606e1350f12055caaefdbdd66e500bc9
A-toys <nao_lespaul_jr@yahoo.co.jp>
2019-12-01 16:40:30 +0900

    add show view files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  # def converting_to_jpy(price)
-  #   "#{price.to_s(:delimited, delimiter: ',')}円"
-  # end
+  def converting_to_jpy(price)
+    "#{price.to_s(:delimited, delimiter: ',')}円"
+  end
 end
