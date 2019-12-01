36aeb95e155546a3c6f06bd2ad26277745eed4bc
A-toys <nao_lespaul_jr@yahoo.co.jp>
2019-12-01 22:35:49 +0900

    add edit and delete functions

--- app/controllers/costs_controller.rb
+++ app/controllers/costs_controller.rb
@@ -9,6 +9,22 @@ class CostsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @cost = Cost.find(params[:id])
+  end
+
+  def update
+    cost = Cost.find(params[:id])
+    cost.update(cost_params)
+    redirect_to "/users/#{current_user.id}"
+  end
+
+  def destroy
+    cost = Cost.find(params[:id])
+    cost.destroy
+    redirect_to "/users/#{current_user.id}"
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'costs#index'
-  resources :costs, only: [:index, :create]
-  resources :users, only: [:show]
+  resources :costs, only: [:index, :create, :edit, :update, :destroy]
+  resources :users, only: :show
 end
